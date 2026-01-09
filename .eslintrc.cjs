70e44882b47ed5f671b727d4c40df1e5c875f451
dingtiancheng@kuaidihelp.com <dingtiancheng@kuaidihelp.com>
2026-01-10 00:30:58 +0800

    chore: 调整项目

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,7 @@ module.exports = {
     sourceType: "module",
   },
   rules: {
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    // 规则：禁止未使用的变量，但允许函数参数以下划线“_”开头（如 _arg），这些参数不会被当作未使用而报错
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
   },
 };
